7ef22bc3441f66174d25e90c480c759ebd9bb61a
yyoel <yoelyulius@gmail.com>
2020-01-29 04:02:29 +0000

    commit

--- src/main/java/bfiAPI/APIController.java
+++ src/main/java/bfiAPI/APIController.java
@@ -2,20 +2,16 @@ package bfiAPI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-
-import java.util.List;
-import java.io.*;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class APIController {
 
     public static Logger logger = LoggerFactory.getLogger(MainApplication.class);
-    private final CountDownLatch latch = new CountDownLatch(3);
 
     @Autowired
     private KafkaTemplate<String, String> template;
@@ -27,5 +23,3 @@ public class APIController {
          logger.info("All Received");
     }
 }
-
-/// kafka-console-consumer --bootstrap-server localhost:9092 --topic transactionTopic --from-beginning
--- src/main/java/bfiAPI/MainApplication.java
+++ src/main/java/bfiAPI/MainApplication.java
@@ -1,18 +1,7 @@
 package bfiAPI;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.context.annotation.Bean;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class MainApplication {
